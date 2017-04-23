0429c6abd4c600ec84dd0d25d7b4373b786a4d6a
Fredrik Skanberg <skanberg@gmail.com>
2017-04-23 19:15:15 +0200

    Added GraphQL.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,33 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { gql, graphql } from 'react-apollo';
 import './App.css';
+
+const Query = gql`
+  query conferenceSchedule {
+    events(slug: "reacteurope-2017") {
+      schedule {
+        title
+        startDate
+        type
+        length
+        speakers {
+          name
+          avatarUrl
+        }
+      }
+    }
+  }
+`;
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        {console.log(this.props.data)}
+        fdsfds
       </div>
     );
   }
 }
 
-export default App;
+export default graphql(Query)(App);
--- src/index.js
+++ src/index.js
@@ -1,9 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import {
+  ApolloClient,
+  createNetworkInterface,
+  ApolloProvider,
+} from "react-apollo";
+import App from "./App";
+import "./index.css";
+
+const networkInterface = createNetworkInterface({
+  uri: "https://www.react-europe.org/gql",
+});
+
+const client = new ApolloClient({
+  networkInterface: networkInterface,
+});
 
 ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById("root"),
 );
