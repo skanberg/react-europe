1ceb3873621d3336d9770354f64be1e96c1f784f
Fredrik Skanberg <skanberg@gmail.com>
2017-04-24 17:01:27 +0200

    Added jsxstyle. Added style and image to loading indicator.

--- src/LoadingIndicator.js
+++ src/LoadingIndicator.js
@@ -1,7 +1,12 @@
 import React from "react";
+import { Col, Block } from "jsxstyle";
+import logo from "./logo.png";
 
 const LoadingIndicator = () => (
-  <div>Loading...</div>
+  <Col height="100%" alignItems="center" justifyContent="center">
+    <img src={logo} width={80} height={80} />
+    <Block fontSize={14} marginTop={20}>Loading...</Block>
+  </Col>
 );
 
 export default LoadingIndicator;
